cdeb8c7b976dcf56805e383e12461bfaf21b3eb9
Zerufael Aemero <zerufaelaemero@gmail.com>
2024-03-13 09:06:00 -0700

    create

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- This script creates and inserts rows into a table
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table (id, name, score) 
+VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
