68f06e4b76555721158aa3b1c621b969666d646a
Zerufael Aemero <zerufaelaemero@gmail.com>
2024-03-13 09:33:49 -0700

    update

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
- -- This script updates a record
-UPDATE second_table
-SET score = 10
-WHERE name = 'Bob';
+-- Updates the score of Bob to 10 in the table second_table.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
