53d0fb313830fa1faa5d9a680f698102b83b0a56
Zerufael Aemero <zerufaelaemero@gmail.com>
2024-03-13 08:49:20 -0700

    Insert

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- This script lists all the rows of a table
+SELECT * FROM first_table;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- This script enters a new row into a table
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
