3fa1bb33e9c5833ee131f2fd665d29f35a134f97
Zerufael Aemero <zerufaelaemero@gmail.com>
2024-04-10 09:59:31 -0700

    108

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv
+
+if (!(parseInt(args[2]))) {
+    console.log('Missing number of occurrences')
+} else {
+    const x = args[2]
+    for (i = 0; i < x; i++) {
+        console.log('C is fun')
+    }
+}
