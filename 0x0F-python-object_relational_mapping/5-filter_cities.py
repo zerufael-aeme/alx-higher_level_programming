cf82f24e4c1a5b4a755acc6120a86f30b10c53d9
Zerufael Aemero <zerufaelaemero@gmail.com>
2024-04-17 09:45:55 -0700

    jgfn

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Merges data from two tables and displays cities 
-   along with their states"""
+"""List all cities that are from 
+the state passed as an argument"""
 
 if __name__ == '__main__':
 
@@ -11,8 +11,11 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("""SELECT cities.name FROM cities INNER JOIN states ON cities.state_id=states.id WHERE states.name = %s
-                ORDER BY cities.id ASC""", (sys.argv[4],))
+    cur.execute("SELECT cities.name\
+                FROM cities LEFT JOIN states\
+                 ON cities.state_id=states.id\
+                WHERE states.name = %s\
+                ORDER BY cities.id ASC", (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
