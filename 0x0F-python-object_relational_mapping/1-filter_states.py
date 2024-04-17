8c5d19f05d000ea4580905e316ea982038b5ad0b
Zerufael Aemero <zerufaelaemero@gmail.com>
2024-04-17 07:26:54 -0700

    eit

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
 
