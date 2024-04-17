1303a1a753d29cde98844481f089c338690ce2d4
Zerufael Aemero <zerufaelaemero@gmail.com>
2024-04-17 07:48:36 -0700

    edit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""lists all states with a name that match the passed argument"""
+"""script that takes argument and displays
+values in the states"""
 
 if __name__ == '__main__':
 
@@ -10,7 +11,8 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name = %s ORDER BY states.id ASC""", (sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                ORDER BY states.id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
