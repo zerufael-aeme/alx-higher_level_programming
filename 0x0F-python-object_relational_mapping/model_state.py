b91618a5a2d1e40d6cec3d703515171c91a3845c
Zerufael Aemero <zerufaelaemero@gmail.com>
2024-04-18 09:14:07 -0700

    model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""class definition of a State and an instance 
-   Base = declarative_base()"""
+"""python file that contains the class definition of
+a State and an instance Base = declarative_base()"""
 
-import SQLAlchemy
-from sqlalchemy.ext.declarative import declarative_base
+from enum import unique
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+Base = declarative_base()
+
 
 class State(Base):
     """Class State"""
 
-    __tablename__ = "states"
- 
-    id = Column(Integer, primary_key=True, autoincrement=True,
-                 nullable=False, unique=True)
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-
